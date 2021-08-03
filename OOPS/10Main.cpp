11a58c82636298c3988378360d246afb675ff656
singh13apoorv <41262394+singh13apoorv@users.noreply.github.com>
2021-08-03 22:10:55 +0530

    Operator overloading part 1

---
+++ OOPS/10Main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include "10OperatorOverloading.cpp"
+using namespace std;
+
+int main(){
+    Fractions f1(10, 2);
+    Fractions f2(15, 4);
+
+    Fractions f3 = f1 * f2;
+    Fractions f4 = f1 + f2;
+
+    f1.print();
+    f2.print();
+    f3.print();
+    f4.print();
+
+    Fractions f5 = f1 * f2;
+    if (f5 == f1){
+        cout << "equal" << endl;
+    }
+    else {
+        cout << "not equal" << endl;
+    }
+
+    return 0;
+}
---
+++ OOPS/10OperatorOverloading.cpp
@@ -0,0 +1,58 @@
+class Fractions{
+    private:
+    int numerator;
+    int denominator;
+
+    public:
+    Fractions(int numerator, int denominator){
+        this->numerator = numerator;
+        this->denominator = denominator;
+    }
+
+    void print(){
+        cout << numerator << " / " << denominator << endl;
+    }
+
+    void simplify(){
+        int gcd = 1;
+        int j = min(numerator, denominator);
+
+        for (int i = 1; i <= j; i++){
+            if (numerator % i == 0 && denominator % i == 0){
+                gcd = i;
+            }
+        }
+
+        numerator /= gcd;
+        denominator /= gcd;
+    }
+
+    //Operator overloading
+    Fractions operator+(Fractions const &f2) const {
+        int lcm = denominator * f2.denominator;
+        int x = lcm / denominator;
+        int y = lcm / f2.denominator;
+        int num = (x * numerator) + (y * f2.numerator);
+
+        Fractions fnew(num, lcm);
+
+        fnew.simplify();
+
+        return fnew;
+    }
+
+    Fractions operator*(Fractions const &f2) const {
+        int num = numerator * f2.numerator;
+        int deno = denominator * f2.denominator;
+
+        Fractions fnew(num, deno);
+
+        fnew.simplify();
+
+        return fnew;
+    }
+
+    bool operator==(Fractions const &f2) const {
+        return (numerator == f2.numerator && denominator == f2.denominator);
+    }
+};
