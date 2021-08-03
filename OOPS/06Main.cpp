2ae2fe9cd0bb71d40f755213a95c487f2a6acce8
singh13apoorv <41262394+singh13apoorv@users.noreply.github.com>
2021-08-03 15:21:22 +0530

    shallow copy and deep copy concept in OOPS

---
+++ OOPS/06Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include "06StudentName.cpp"
+using namespace std;
+
+int main(){
+    char name[] = "Apoorv";
+    Student s1(21 , name);
+    s1.display();
+
+    return 0;
+}
