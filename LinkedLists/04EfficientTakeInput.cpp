777325800935fe75760bada0283b61f0b11b0348
singh13apoorv <41262394+singh13apoorv@users.noreply.github.com>
2021-08-07 16:36:34 +0530

    efficient way of taking input in linked list from user using tail pointer

---
+++ LinkedLists/04EfficientTakeInput.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this -> data = data;
+        next = NULL;
+    }
+};
+
+Node* TakeInput(){
+    int data;
+    cout << "Enter first data: ";
+    cin >> data;
+    Node* head = NULL;
+    Node* tail = NULL;
+    string input = "yes";
+
+    while (input == "yes"){
+        Node* newNode = new Node(data);
+        if (head == NULL){
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail -> next = newNode;
+            tail = newNode;
+        }
+        cout << "Enter the data: ";
+        cin >> data;
+        cout << "Enter your decision(yes, no): ";
+        cin >> input;
+    }
+
+    return head;
+}
+
+void print(Node* head){
+    Node* temp = head;
+
+    while (temp != NULL){
+        cout << temp -> data << "->";
+        temp = temp -> next;
+    }
+    cout << "NULL" << endl;
+}
+
+
+int main(){
+    Node* head = TakeInput();
+    print(head);
+
+    return 0;
+}
