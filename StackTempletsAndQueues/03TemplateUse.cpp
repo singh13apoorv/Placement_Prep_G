fb41d98ae47b452e051c6c3b7e9e06616b240eb7
singh13apoorv <41262394+singh13apoorv@users.noreply.github.com>
2021-08-12 15:40:40 +0530

    creating and using template in c++

---
+++ StackTempletsAndQueues/03TemplateUse.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "03TemplateEx.cpp"
+using namespace std;
+
+int main(){
+    Pair<int, double> p1;
+
+    p1.setX(100);
+    p1.setY(100.23);
+
+    cout << p1.getX() << endl;
+    cout << p1.getY() << endl;
+
+    return 0;
+}
