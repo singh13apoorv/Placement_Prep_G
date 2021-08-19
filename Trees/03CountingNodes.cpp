e49086ca3c80d17bddbd5685f382936eb70a3f06
singh13apoorv <41262394+singh13apoorv@users.noreply.github.com>
2021-08-19 20:10:18 +0530

    counting number of nodes in a tree

---
+++ Trees/03CountingNodes.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include "TreeNode.h"
+using namespace std;
+
+TreeNode<int>* takeInputLevelWise(){
+    int rootData;
+    cout << "Enter the root data: ";
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>* > pendingNodes;
+    pendingNodes.push(root);
+
+    while (pendingNodes.size() != 0){
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+
+        int numChild;
+        cout << "Enter number of children of " << front -> data << ": ";
+        cin >> numChild;
+
+        for (int i = 0; i < numChild; i++){
+            int childData;
+            cout << "Enter the data for " << i << "th child: ";
+            cin >> childData;
+
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front -> children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int CountNodes(TreeNode<int>* root){
+    int ans = 1;
+
+    for (int i = 0; i < root -> children.size(); i++){
+        ans += CountNodes(root -> children[i]);
+    }
+
+    return ans;
+}
+
+int main(){
+    TreeNode<int>* root = takeInputLevelWise();
+    cout << CountNodes(root) << endl;
+
+    return 0;
+}
