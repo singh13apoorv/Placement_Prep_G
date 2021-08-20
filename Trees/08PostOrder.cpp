9b86c9cbb5b6e7e2d5285e405cd8c36ce5e30afe
singh13apoorv <41262394+singh13apoorv@users.noreply.github.com>
2021-08-20 17:50:41 +0530

    postorder traversal in tree

---
+++ Trees/08PostOrder.cpp
@@ -0,0 +1,55 @@
+//1 3 2 3 4 2 5 6 2 7 8 0 0 0 0 1 9 0
+
+#include <bits/stdc++.h>
+#include "TreeNode.h"
+using namespace std;
+
+TreeNode<int>* takeInputLevelWise(){
+    int rootData;
+    cout << "Enter the root data: ";
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>* > pendingNodes;
+    pendingNodes.push(root);
+
+    while (pendingNodes.size() != 0){
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+
+        int numChild;
+        cout << "Enter number of children of " << front -> data << ": ";
+        cin >> numChild;
+
+        for (int i = 0; i < numChild; i++){
+            int childData;
+            cout << "Enter the data for " << i << "th child: ";
+            cin >> childData;
+
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front -> children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+void printPostOrder(TreeNode<int>* root) {
+    // Write your code here
+    if (root == NULL){
+        return;
+    }
+    for (int i = 0; i < root -> children.size(); i++){
+        printPostOrder(root -> children[i]);
+    }
+    
+    cout << root -> data << " ";
+}
+
+int main(){
+    TreeNode<int>* root = takeInputLevelWise();
+    printPostOrder(root);
+
+    return 0;
+}
