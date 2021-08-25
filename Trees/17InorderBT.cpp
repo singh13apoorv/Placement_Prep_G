5173d84b1d4ead3c1b1fe797d732566b5f5f11fb
singh13apoorv <41262394+singh13apoorv@users.noreply.github.com>
2021-08-25 16:24:53 +0530

    inorder traversal in binary trees

---
+++ Trees/17InorderBT.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <queue>
+#include "BinaryTreeNode.h"
+using namespace std;
+
+BinaryTreeNode<int>* takeInputLevelWise(){
+    int rootData;
+    cout << "Enter root data:";
+    cin >> rootData;
+
+    if (rootData == -1){
+        return NULL;
+    }
+
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>* > pendingNodes;
+
+    pendingNodes.push(root);
+
+    while (pendingNodes.size() != 0){
+        BinaryTreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+
+        int lc;
+        cout << "Enter left child for " << front -> data << " :";
+        cin >> lc;
+        if (lc != -1){
+            BinaryTreeNode<int>* lchild = new BinaryTreeNode<int>(lc);
+            front -> leftChild = lchild;
+            pendingNodes.push(lchild);
+        }
+
+        int rc;
+        cout << "Enter right child for " << front -> data << " :";
+        cin >> rc;
+        if (rc != -1){
+            BinaryTreeNode<int>* rchild = new BinaryTreeNode<int>(rc);
+            front -> rightChild = rchild;
+            pendingNodes.push(rchild);
+        }
+    }
+
+    return root;
+}
+
+void inOrder(BinaryTreeNode<int>* root){
+    if (root == NULL){
+        return;
+    }
+
+    inOrder(root -> leftChild);
+    cout << root -> data << " ";
+    inOrder(root -> rightChild);
+}
+
+int main(){
+    BinaryTreeNode<int>* root = takeInputLevelWise();
+    inOrder(root);
+    delete root;
+
+    return 0;
+}
